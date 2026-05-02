4a1199cf589176f82197c9f1b9fafeeff3a4f63e
d <dhiru579@gmail.com>
2026-05-02 20:44:08 +0530

    test: added test for base10 inputs conversions

---
+++ tests/test.rs
@@ -0,0 +1,70 @@
+use parameterized::parameterized;
+
+use numeral_system_rs::NumberWithBase;
+
+#[parameterized(
+    test_case={
+        // ()
+        (1, "10", "provided base is not supported"),
+        (2, "10", "1010"),
+        (3, "10", "101"),
+        (3, "11", "102"),
+        (4, "10", "22"),
+        (5, "10", "20"),
+        (6, "10", "14"),
+        (7, "10", "13"),
+        (8, "10", "12"),
+        (9, "10", "11"),
+        (10, "10", "10"),
+        (11, "10", "A"),
+        (12, "11", "B"),
+        (13, "12", "C"),
+        (14, "13", "D"),
+        (15, "14", "E"),
+        (16, "15", "F"),
+        (16, "1008732", "F645C"),
+        (17, "16", "provided base is not supported"),
+        (16, "abc", "Value is not a number"),
+    }
+)]
+fn test_base10_data(test_case: (u8, &str, &str)) {
+    let (out_base, inp_val, expected) = test_case;
+    let cust_base_number: Result<NumberWithBase, String> =
+        NumberWithBase::from_base10(inp_val.to_string());
+    match cust_base_number {
+        Ok(mut numx) => {
+            match numx.mutate_to_base_n(out_base) {
+                Err(x) => {
+                    assert_eq!(expected, x);
+                }
+                Ok(_) => {
+                    assert_eq!(numx.get_base(), out_base);
+                    assert_eq!(numx.get_value(), expected);
+                }
+            };
+        }
+        Err(my_str) => {
+            assert_eq!(expected.to_string(), my_str);
+        }
+    }
+
+    let cust_base_number2: Result<NumberWithBase, String> =
+        NumberWithBase::from_base10(inp_val.to_string());
+    match cust_base_number2 {
+        Ok(numx) => {
+            let newnum: Result<NumberWithBase, String> = numx.convert_to_base_n(out_base);
+            match newnum {
+                Err(x) => {
+                    assert_eq!(expected, x);
+                }
+                Ok(numy) => {
+                    assert_eq!(numy.get_base(), out_base);
+                    assert_eq!(numy.get_value(), expected);
+                }
+            };
+        }
+        Err(my_str) => {
+            assert_eq!(expected.to_string(), my_str);
+        }
+    }
+}
