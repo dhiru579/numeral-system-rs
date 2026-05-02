9323f731109253c804538a18d5a2db9b8be72ff8
d <dhiru579@gmail.com>
2026-05-02 20:38:02 +0530

    add: method to mutate curr.struct to diffrent base

--- src/lib.rs
+++ src/lib.rs
@@ -34,6 +34,47 @@ impl NumberWithBase {
     pub fn get_base_and_value(self: &NumberWithBase) -> (u8, String) {
         (self.get_base(), self.get_value())
     }
+
+    fn get_base_and_value_post_base_update(
+        base: u8,
+        value: &String,
+    ) -> Result<(u8, String), String> {
+        check_if_base_allowed(&base)?;
+
+        let inp: u32 = parse_string_to_int(value)?;
+
+        let mut out_str: String = String::new();
+        let mut temp: u32 = inp;
+        while temp > 1_u32 {
+            let digx: u32 = temp % base as u32;
+            let charx: String = if base > 9 {
+                match CHAR_DIGIT_MAP_DIGITS.iter().position(|&x| x == digx as u8) {
+                    Some(x) => match CHAR_DIGIT_MAP_CHARS.get(x) {
+                        Some(y) => y.to_string(),
+                        None => digx.to_string(),
+                    },
+                    None => digx.to_string(),
+                }
+            } else {
+                digx.to_string()
+            };
+            out_str.insert_str(0, &charx);
+            temp /= base as u32;
+        }
+        if temp == 1_u32 {
+            out_str.insert_str(0, &temp.to_string());
+        }
+
+        return Ok((base, out_str));
+    }
+
+    pub fn mutate_to_base_n(self: &mut NumberWithBase, base: u8) -> Result<(), String> {
+        let (base, out_str) = Self::get_base_and_value_post_base_update(base, &self.value)?;
+
+        self.base = base;
+        self.value = out_str;
+        Ok(())
+    }
 }
 
 fn parse_string_to_int(val: &String) -> Result<u32, String> {
