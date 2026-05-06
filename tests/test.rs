16aea7909333ea23b067456fa19d8a81d01acf34
d <dhiru579@gmail.com>
2026-05-06 22:17:52 +0530

    test: input & output base are equal

--- tests/test.rs
+++ tests/test.rs
@@ -59,6 +59,7 @@ fn test_base10_data(test_case: (u8, &str, &str)) {
         (10, "4567", 2, "1000111010111"),
         (12, "1B0", 8, "424"),
         (16, "1ff0", 13, "394C"),
+        (14, "1bc0", 14, "1bc0"),
         (18, "1ff0", 13, "provided base is not supported"),
     }
 )]
