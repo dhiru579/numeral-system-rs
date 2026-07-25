0f21cad1eec655de1786fa20b61626cf4d0f6a88
d <dhiru579@gmail.com>
2026-07-25 20:11:08 +0530

    feat: output_type in cli params

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use clap::Parser;
 use numeral_system_rs::NumberWithBase;
+mod output;
+use output::{ConvertionResult, OutputType};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -13,6 +15,10 @@ struct Args {
     /// convert to base
     #[arg(short, default_value_t = 10_u8)]
     output_base: u8,
+    
+    /// type of the output
+    #[arg(value_enum, default_value_t = OutputType::Verbose)]
+    output_type: OutputType,
 }
 
 fn main() {
@@ -21,28 +27,42 @@ fn main() {
     let num: Result<NumberWithBase, String> =
         NumberWithBase::from(data.input_base, data.input_value.clone());
 
+    let mut conversion_res: ConvertionResult = ConvertionResult::new(data.input_base, data.input_value.clone());
+
     match num {
         Ok(mut num1) => match num1.mutate_to_base_n(data.output_base) {
             Ok(_) => {
+                conversion_res.set_true(num1.get_base(), num1.get_value());
+            }
+            Err(strx) => {
+                conversion_res.set_false(strx);
+            }
+        },
+        Err(strx) => {
+            conversion_res.set_false(strx);
+        }
+    }
+
+    match data.output_type {
+        OutputType::Verbose => {
+            if conversion_res.get_status() {
                 println!("status => PASS");
                 println!(
                     "input  => value = \"{}\"\tbase = \"{}\"",
                     data.input_value, data.input_base
                 );
+                let (out_base, out_val) = conversion_res.get_output();
                 println!(
-                    "output => value = \"{}\"\tbase = \"{}\"",
-                    num1.get_value(),
-                    num1.get_base()
+                    "output => value = \"{}\"\tbase = \"{}\"", out_base, out_val
                 );
-            }
-            Err(strx) => {
+            } else {
                 println!("status => FAIL");
-                println!("reason => {}", strx);
+                println!("reason => {}", conversion_res.get_reason());
             }
         },
-        Err(strx) => {
-            println!("status => FAIL");
-            println!("reason => {}", strx);
+        _ => {
+            println!("NOTE: this OutputType is yet be implemented");
+            todo!();
         }
     }
 }
---
+++ src/output.rs
@@ -0,0 +1,49 @@
+use clap::ValueEnum;
+
+#[derive(Debug)]
+pub struct ConvertionResult {
+    status: bool,
+    reason: String,
+    input: (String, String),
+    output: (String, String),
+}
+
+impl ConvertionResult {
+    pub fn new(base: u8, value: String) -> Self {
+        ConvertionResult {
+            status: false,
+            reason: "-".to_string(),
+            input: (base.to_string(), value),
+            output: ("-".to_string(), "-".to_string()),
+        }
+    }
+
+    pub fn set_true(self: &mut ConvertionResult, output_base: u8, output_val: String) {
+        self.status = true;
+        self.output = (output_base.to_string(), output_val);
+    }
+
+    pub fn set_false(self: &mut ConvertionResult, reason: String) {
+        self.reason = reason;
+    }
+
+    pub fn get_status(self: &ConvertionResult) -> bool {
+        self.status
+    }
+
+    pub fn get_output(self: &ConvertionResult) -> (String, String) {
+        (self.output.0.to_string(), self.output.1.clone())
+    }
+
+    pub fn get_reason(self: &ConvertionResult) -> String {
+        self.reason.clone()
+    }
+}
+
+#[derive(Debug, Clone, ValueEnum)]
+pub enum OutputType {
+    Csv,
+    Json,
+    Verbose,
+    Clean,
+}
